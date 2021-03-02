c86b0df038b77305a07a3d1beccac7bef7baff30
Carlos Eduardo Cassaniga Forti <carlosforti@carlosforti.com.br>
2021-03-01 23:20:41 -0300

    Correção de configuração pra SQLServer

--- src/MinhaBiblioteca.Api/Startup.cs
+++ src/MinhaBiblioteca.Api/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.OpenApi.Models;
 using MinhaBiblioteca.API.Formatter;
 using MinhaBiblioteca.Application.Configurations;
 using MinhaBiblioteca.Infra.Data.Configurations;
+using MinhaBiblioteca.Infra.Data.Context;
 using MinhaBiblioteca.Infra.Shared.Configurations;
 
 namespace MinhaBiblioteca.API
--- src/MinhaBiblioteca.Infra.Data/Context/BibliotecaContext.cs
+++ src/MinhaBiblioteca.Infra.Data/Context/BibliotecaContext.cs
@@ -1,7 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.DependencyInjection;
 using MinhaBiblioteca.Domain.Entities;
 using MinhaBiblioteca.Infra.Data.Views;
 
@@ -9,13 +11,9 @@ namespace MinhaBiblioteca.Infra.Data.Context
 {
     public class BibliotecaContext : DbContext
     {
-        public DbSet<Editora> Editoras { get; set; }
-        public DbSet<Autor> Autores { get; set; }
-        public DbSet<LivroView> Livros { get; set; }
-
-        public BibliotecaContext(DbContextOptions options)
-            : base(options)
+        public BibliotecaContext([NotNull] DbContextOptions options) : base(options)
         {
+            this.Database.EnsureCreated();
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -24,20 +22,8 @@ namespace MinhaBiblioteca.Infra.Data.Context
             base.OnModelCreating(modelBuilder);
         }
 
-        public async Task SaveChangesAsync()
-        {
-            await base.SaveChangesAsync();
-        }
-
-        public override EntityEntry<T> Entry<T>(T entity)
-            where T : class
-        {
-            return base.Entry<T>(entity);
-        }
-
-        public new void Remove<T>(T entity) where T : class
-        {
-            base.Remove(entity);
-        }
+        public DbSet<Editora> Editoras { get; set; }
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<LivroView> Livros { get; set; }
     }
 }
